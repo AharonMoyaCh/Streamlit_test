edc5e8d36b9639859a8df954368b1f7a7c3742f1
AharonMoyaCh <Aharon_Moya_Chavarria@mckinsey.com>
2025-11-16 11:12:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import requests
 import json
+import pandas as pd
+from databricks import sql
 
-def score_model(question):
+def get_query(question):
     url = 'https://dbc-164e54c4-ef63.cloud.databricks.com/serving-endpoints/Final_Testing/invocations'
     headers = {'Authorization': f'Bearer {"dapi7aff94b92948d811e9f9019293f4705d"}', 'Content-Type': 'application/json'}
     data_json = {'inputs': [question]}
@@ -11,13 +13,31 @@ def score_model(question):
         raise Exception(f'Request failed with status {response.status_code}, {response.text}')
     return response.json()
 
+def run_query(sql_query):
+    # Conexión al workspace Databricks
+    connection = sql.connect(
+        server_hostname="dbc-164e54c4-ef63.cloud.databricks.com",
+        http_path="/sql/1.0/warehouses/591de624a7eca260",  
+        access_token="dapi7aff94b92948d811e9f9019293f4705d"
+    )
+    cursor = connection.cursor()
+    cursor.execute(sql_query)
+    columns = [col[0] for col in cursor.description]
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return pd.DataFrame(data, columns=columns)
+
 st.title("Demo: Databricks model from Streamlit")
 user_input = st.text_input("Ask your question")
 if st.button("Send"):
     try:
-        result = score_model(user_input)
+        result = get_query(user_input)
         sql_query = result['predictions'][0]['sql']
+        data = run_query(sql_query)
         st.subheader("Generated SQL Query:")
         st.code(sql_query, language='sql')
+        st.subheader("Query Result:")
+        st.dataframe(data)
     except Exception as e:
         st.error(f"Error: {e}")
