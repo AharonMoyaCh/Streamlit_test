662f05c853d1377fbd453c3d1824a2f031a54b65
AharonMoyaCh <Aharon_Moya_Chavarria@mckinsey.com>
2025-11-16 09:26:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,16 @@ import streamlit as st
 import requests
 import json
 
-# Config
-DATABRICKS_URL = "https://dbc-164e54c4-ef63.cloud.databricks.com/serving-endpoints/Final_Testing/invocations"
-TOKEN = "dapi7aff94b92948d811e9f9019293f4705d"
+def score_model(question):
+    url = 'https://dbc-164e54c4-ef63.cloud.databricks.com/serving-endpoints/Final_Testing/invocations'
+    headers = {'Authorization': f'Bearer {"dapi7aff94b92948d811e9f9019293f4705d"}', 'Content-Type': 'application/json'}
+    data_json = {'inputs': [question]}
+    response = requests.request(method='POST', headers=headers, url=url, data=json.dumps(data_json))
+    if response.status_code != 200:
+        raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+    return response.json()
 
 st.title("Demo: Databricks model from Streamlit")
-
 user_input = st.text_input("Ask your question")
 if st.button("Send"):
-    headers = {
-        "Authorization": f"Bearer {TOKEN}",
-        "Content-Type": "application/json"
-    }
-
-    data = {
-        "input": [user_input]
-    }
-
-    response = requests.post(DATABRICKS_URL, headers=headers, data=json.dumps(data))
-
-    if response.status_code == 200:
-        result = response.json()
-        st.write("Result:", result)
-    else:
-        st.error(f"Error: {response.status_code}")
-        st.write(response.text)
+    score_model(question)
